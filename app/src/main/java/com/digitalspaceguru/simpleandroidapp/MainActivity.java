b30d25a7525351121a70180afd837ec6c7f82444
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2022-05-23 14:07:39 +0300

    invoke the dialog as a click event

--- app/src/main/java/com/digitalspaceguru/simpleandroidapp/MainActivity.java
+++ app/src/main/java/com/digitalspaceguru/simpleandroidapp/MainActivity.java
@@ -4,16 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.moodButton1) Button moodButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        MoodDialogFragment moodDialogFragment = new MoodDialogFragment();
-        moodDialogFragment.show(fragmentManager, "Sample Fragment");
+        moodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                MoodDialogFragment moodDialogFragment = new MoodDialogFragment();
+                moodDialogFragment.show(fragmentManager, "Sample Fragment");
+
+            }
+        });
     }
 }
