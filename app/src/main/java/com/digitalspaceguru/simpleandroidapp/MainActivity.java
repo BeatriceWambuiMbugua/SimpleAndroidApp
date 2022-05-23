0e681f35eecfe2a4d1086d5db3a52ce557200584
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2022-05-23 13:35:26 +0300

    invoke fragment

--- app/src/main/java/com/digitalspaceguru/simpleandroidapp/MainActivity.java
+++ app/src/main/java/com/digitalspaceguru/simpleandroidapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.digitalspaceguru.simpleandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MoodDialogFragment moodDialogFragment = new MoodDialogFragment();
+        moodDialogFragment.show(fragmentManager, "Sample Fragment");
     }
 }
