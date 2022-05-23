1d2989b12b559ecdb7667d8c71789239741fbe6b
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2022-05-23 14:45:33 +0300

    return values from the form

--- app/src/main/java/com/digitalspaceguru/simpleandroidapp/MoodDialogFragment.java
+++ app/src/main/java/com/digitalspaceguru/simpleandroidapp/MoodDialogFragment.java
@@ -4,15 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.fragment.app.DialogFragment;
 
 public class MoodDialogFragment extends DialogFragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-     View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
-    getDialog().setTitle("Simple Dialog");
-    return rootView;
+        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
+        getDialog().setTitle("Simple Dialog");
+
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+        return rootView;
 
     }
 }
