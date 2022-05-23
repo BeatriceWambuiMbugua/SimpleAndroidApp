f0e2bfa3f00bb234fc6684758bd10b53fe773490
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2022-05-23 14:47:00 +0300

    import log class

--- app/src/main/java/com/digitalspaceguru/simpleandroidapp/MoodDialogFragment.java
+++ app/src/main/java/com/digitalspaceguru/simpleandroidapp/MoodDialogFragment.java
@@ -1,10 +1,13 @@
 package com.digitalspaceguru.simpleandroidapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.fragment.app.DialogFragment;
 
@@ -19,9 +22,22 @@ public class MoodDialogFragment extends DialogFragment {
         Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
         Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
 
+        RadioGroup surveyRadioGroup = (RadioGroup) rootView.findViewById(R.id.moodRadioGroup); //pull group
+        int selectedId = surveyRadioGroup.getCheckedRadioButtonId(); //get selected ID
+        final RadioButton selectedRadioButton = (RadioButton) rootView.findViewById(selectedId); //get r button val via ID
+
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d("testing", selectedRadioButton.getText().toString());
                 dismiss();
             }
         });
